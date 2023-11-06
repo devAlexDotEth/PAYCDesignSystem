08bdeee7446793747891281cfd35bcb1664670b8
Rollablazer <rollablazer@gmail.com>
2023-11-06 17:58:43 +1100

    Removed unnecessary pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import { Container } from './styles';
-import Home from './compositions/home';
-// import PortalTemplate from './compositions/portal-template';
-// import Portals from './compositions/portals';
-// import Contact from './compositions/contact';
-// import DesignSystem from './compositions/design-system';
+import DesignSystem from './compositions/design-system';
 
 function App() {
   
   return (
     <Container>
-      <Home />
-      {/* <Portals /> */}
-      {/* <DesignSystem /> */}
-      {/* <Contact /> */}
-      {/* <PortalTemplate /> */}
+      <DesignSystem />
     </Container>
   );
 }
